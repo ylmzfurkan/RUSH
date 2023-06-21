ad486cedb60372600d77063050b4cde8f6df0763
Furkan Yılmaz <ylmzfurkann@gmail.com>
2023-06-21 12:51:03 +0300

    yeni .html eklendi

--- Python-django-rush/movieapp/movies/urls.py
+++ Python-django-rush/movieapp/movies/urls.py
@@ -8,8 +8,8 @@ from . import views
 #http://127.0.0.1:8000/movies/the-walking-dead
 
 urlpatterns = [
-    path('', views.home ),
+    path('', views.home, name="home"),
     path('home', views.home ),
-    path('movies', views.movies ),
-
+    path('movies', views.movies, name="movies" ),
+    path('movies/<int:id>', views.moviedetails, name="details" ),
     ]
--- Python-django-rush/movieapp/movies/views.py
+++ Python-django-rush/movieapp/movies/views.py
@@ -3,24 +3,28 @@ from django.shortcuts import render
 kategori_liste = ['macera' , 'romantik' , 'dram' , 'bilim kurgu']
 film_liste = [
     {
+        "id": 1,
         "film_adi": "film 1",
         "aciklama": "film 1 açıklama",
         "resim": "1.jpeg",
         "anasayfa": True
     },
     {
+        "id": 2,
         "film_adi": "film 2",
         "aciklama": "film 2 açıklama",
         "resim": "2.jpeg",
         "anasayfa": True
     },
     {
+        "id": 3,
         "film_adi": "film 3",
         "aciklama": "film 3 açıklama",
         "resim": "3.jpeg",
         "anasayfa": False
     },
     {
+        "id": 4,
         "film_adi": "film 4",
         "aciklama": "film 4 açıklama",
         "resim": "4.jpeg",
@@ -42,3 +46,9 @@ def movies(request):
         'filmler': film_liste,
     }
     return render(request, 'movies.html', data)
+
+def moviedetails(request, id):
+    data = {
+        "id": id
+    }
+    return render(request, 'details.html', data)
